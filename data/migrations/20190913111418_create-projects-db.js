91b97cf02728e6f7ca3d987b4f18919aa9090e2e
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2019-09-13 11:53:21 -0400

    Add database schema

--- data/migrations/20190913111418_create-projects-db.js
+++ data/migrations/20190913111418_create-projects-db.js
@@ -1,8 +1,50 @@
-
 exports.up = function(knex) {
-  
+    return knex.schema
+        .createTable('projects', tbl => {
+            tbl.increments();
+            tbl.string('name', 512)
+                .notNullable()
+                .unique();
+            tbl.text('description');
+            tbl.boolean('completed').defaultTo(false);
+        })
+        .createTable('resources', tbl => {
+            tbl.increments();
+            tbl.string('name', 255)
+                .notNullable()
+                .unique();
+            tbl.text('description');
+        })
+        .createTable('project_resources', tbl => {
+            tbl.increments();
+            tbl.integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('projects')
+                .onDelete('RESTRICT')
+                .onUpdate('CASCADE');
+            tbl.integer('resource_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('resources')
+                .onDelete('RESTRICT')
+                .onUpdate('CASCADE');
+        })
+        .createTable('tasks', tbl => {
+            tbl.increments();
+            tbl.integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('projects')
+                .onDelete('RESTRICT')
+                .onUpdate('CASCADE');
+            tbl.text('description').notNullable();
+            tbl.text('notes');
+            tbl.boolean('completed').defaultTo(false);
+        });
 };
 
-exports.down = function(knex) {
-  
-};
+exports.down = function(knex) {};
